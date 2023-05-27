aa45ec89d7e79eebcf7adf80f7c7b0bbf806e4b6
aivanov <alex130896@gmail.com>
2023-05-27 23:17:26 +0400

    Initial Commit

---
+++ iOS10-HW10-Alexandr-Ivanov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS10-HW10-Alexandr-Ivanov
+//
+//  Created by Александр Иванов on 27.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
